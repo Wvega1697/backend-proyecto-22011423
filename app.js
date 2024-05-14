a55ca5751d2045db468e3ff3eaa709db2f25f7e3
WVEGA1697 <williamsv1697@gmail.com>
2024-05-13 23:24:11 -0600

    ADD
    Se agrega CORS y se mueve al puerto 3001. Ya se tenian los codigos 200, 400 y 401 implementados correctamente.
    
    Se elimina archivo users.js y sus referencias.

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var goalsRouter = require('./routes/goals');
 var tasksRouter = require('./routes/tasks');
 const router = express.Router();
+var cors = require('cors');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors())
 app.use('/', router);
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,7 +31,6 @@ router.use((req, res, next) => {
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/goals', goalsRouter);
 app.use('/tasks', tasksRouter);
 
